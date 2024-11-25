ea1ab0476ebff698b2335682d4437cc76733994b
kauangame21 <00001096642451sp@al.educacao.sp.gov.br>
2024-11-25 20:49:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'fisica',
+    'O que é fisica?',
+    'A Física é o campo da ciência que investiga os fenômenos'
+)
+
+criaCartao(
+    'quimica',
+    'o que é quimica?',
+    'Química é a ciência que estuda a matéria, sua estrutura'
+)
+
+criaCartao(
+    'matematica',
+    'o que é matematica?',
+    'matemática é uma área do conhecimento que inclui os tópicos'
+)
+
+criaCartao(
+    'educação fisica ',
+    'o que é educação fisica?',
+    'Educação Física está historicamente atrelada'
+)
